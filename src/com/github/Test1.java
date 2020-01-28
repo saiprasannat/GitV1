a528e3507cb6d31cc55c78eec168c4268eb2941d
SaiPrasannaThota <prasanna94.thota@gmail.com>
2020-01-28 17:02:52 +0530

    First Commit...

---
+++ src/com/github/Test1.java
@@ -0,0 +1,12 @@
+package com.github;
+
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hi Sai!! This is Test1 class");
+	}
+
+}
