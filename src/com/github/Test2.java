35ea9a9d56cefc7e75305aa9578412f1db9a474a
SaiPrasannaThota <prasanna94.thota@gmail.com>
2020-01-28 17:28:55 +0530

    Commiting the test2 class

---
+++ src/com/github/Test2.java
@@ -0,0 +1,12 @@
+package com.github;
+
+public class Test2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hi Sai!! This is Test2 class and I am a developer");
+	}
+
+}
